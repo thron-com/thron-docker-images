47ae5bf35881a9e1cdb941f1d3a842946414a5b5
Luca Agostini <luca.agostini@newvision.it>
2016-10-13 10:33:34 +0200

    ADD - added php conf

--- php/5.5-fpm-nginx/files/etc/docker/start.sh
+++ php/5.5-fpm-nginx/files/etc/docker/start.sh
@@ -1,2 +1,16 @@
 #!/bin/bash
-/usr/bin/supervisord -n -c /etc/supervisord.conf
+if [ -z "${PHP_UPLOAD_MAX_FILESIZE}" ]; then PHP_UPLOAD_MAX_FILESIZE="512M"; fi
+if [ -z "${PHP_MAX_FILE_UPLOADS}" ]; then PHP_MAX_FILE_UPLOADS="20"; fi
+if [ -z "${PHP_POST_MAX_SIZE}" ]; then PHP_POST_MAX_SIZE="512M"; fi
+if [ -z "${PHP_DISPLAY_ERRORS}" ]; then PHP_DISPLAY_ERRORS="Off"; fi
+if [ -z "${PHP_MAX_INPUT_TIME}" ]; then PHP_MAX_INPUT_TIME="60"; fi
+if [ -z "${PHP_TIMEZONE}" ]; then PHP_TIMEZONE="Etc/UTC"; fi
+
+export PHP_UPLOAD_MAX_FILESIZE
+export PHP_MAX_FILE_UPLOADS
+export PHP_POST_MAX_SIZE
+export PHP_DISPLAY_ERRORS
+export PHP_MAX_INPUT_TIME
+export PHP_TIMEZONE
+
+/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
