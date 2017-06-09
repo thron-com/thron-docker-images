0be35110ae14a6c86ff914408761f440ab0b9229
luca.agostini <luca.agostini@thron.com>
2017-06-09 18:10:43 +0200

    CHANGE - moved the script provisioning only in the master node. Slaves downloads the script from the master

--- locustio/files/etc/docker/start.sh
+++ locustio/files/etc/docker/start.sh
@@ -18,21 +18,30 @@ then
     LOCUSTIO_HOST="http://127.0.0.1"
 fi
 
-if [ -z "${LOCUSTIO_SCRIPT}" ];
-then
-    echo "Script not set, please set the LOCUSTIO_SCRIPT variable"
-    exit 1
-else
-    echo "Downloading script"
-    wget -O ${LOCUSTIO_PATH}/script.py ${LOCUSTIO_SCRIPT}
-fi
 
-if [ ! -z "${LOCUSTIO_MASTER}" ];
+if [ "${LOCUSTIO_MASTER}" ];
 then
     echo "Running slave"
+    echo "Downloading script from the master"
+    wget -O ${LOCUSTIO_PATH}/script.py http://${LOCUSTIO_MASTER}/script.py
+    echo "Running LocustIO"
     locust -f ${LOCUSTIO_PATH}/script.py --slave --master-host=${LOCUSTIO_MASTER} --host=${LOCUSTIO_HOST}
 else
     echo "Running master"
+    
+    if [ -z "${LOCUSTIO_SCRIPT}" ];
+    then
+        echo "Script not set, please set the LOCUSTIO_SCRIPT variable"
+        exit 1
+    else
+        echo "Downloading script"
+        wget -O ${LOCUSTIO_PATH}/script.py ${LOCUSTIO_SCRIPT}
+    fi
+
+    echo "Running Nginx"
+    nginx -c /etc/nginx/nginx.conf
+    
+    echo "Running LocustIO"
     locust -f ${LOCUSTIO_PATH}/script.py --master --host=${LOCUSTIO_HOST}
 fi
 
