962cf1177983585bf89e3efd8e4505acaaa53498
Luca Agostini <agostini.luca@gmail.com>
2018-11-23 12:13:42 +0100

    Add PROMETHEUS_WEB_EXTERNAL_URL param in prometheus

--- prometheus/files/etc/docker/start.sh
+++ prometheus/files/etc/docker/start.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
 echo "PROMETHEUS_CONF_URI: set the prometheus URI configuration"
-echo "PROMETHEUS_RETENTION: set the prometheus retention. The default value i 15d"
+echo "PROMETHEUS_RETENTION: set the prometheus retention. The default value is '15d'"
+echo "PROMETHEUS_WEB_EXTERNAL_URL: set the prometheus external URL. The default value is '/'"
 
 if [ -z "${PROMETHEUS_RETENTION}" ]; then 
   echo "Using PROMETHEUS_RETENTION='90d'"
   PROMETHEUS_RETENTION="90d" 
+fi
+if [ -z "${PROMETHEUS_WEB_EXTERNAL_URL}" ]; then 
+  echo "Using PROMETHEUS_WEB_EXTERNAL_URL='/'"
+  PROMETHEUS_WEB_EXTERNAL_URL="/" 
 fi
 
 
@@ -26,7 +31,7 @@ fi
 
 exec /bin/prometheus \
   --config.file=/etc/prometheus/prometheus.yml \
-  --web.external-url=/management/prometheus \
+  --web.external-url=${PROMETHEUS_WEB_EXTERNAL_URL} \
   --storage.tsdb.retention=${PROMETHEUS_RETENTION} \
   --storage.tsdb.path=/prometheus \
   --web.console.libraries=/etc/prometheus/console_libraries \
